7dd92529571c3a8fec7f88adc42b4fbd8ffd4550
Jacob Bare <jacob@limit0.io>
2019-05-22 16:09:47 -0500

    Create env file and required vars

---
+++ services/graphql/src/env.js
@@ -0,0 +1,24 @@
+const {
+  cleanEnv,
+  makeValidator,
+  port,
+} = require('envalid');
+
+const nonemptystr = makeValidator((v) => {
+  const err = new Error('Expected a non-empty string');
+  if (v === undefined || v === null || v === '') {
+    throw err;
+  }
+  const trimmed = String(v).trim();
+  if (!trimmed) throw err;
+  return trimmed;
+});
+
+module.exports = cleanEnv(process.env, {
+  TENANT_KEY: nonemptystr({ desc: 'The tenant key. Is used for querying the account information and settings from the core database connection.' }),
+  TENANT_DSN: nonemptystr({ desc: 'The MongoDB tenant DSN to connect to.' }),
+  CORE_DSN: nonemptystr({ desc: 'The MongoDB core DSN to connect to.' }),
+  REDIS_DSN: nonemptystr({ desc: 'The Redis DSN to connect to.' }),
+  INTERNAL_PORT: port({ desc: 'The internal port that express will run on.', default: 80 }),
+  EXTERNAL_PORT: port({ desc: 'The external port that express is exposed on.', default: 80 }),
+});
--- services/graphql/src/index.js
+++ services/graphql/src/index.js
@@ -1,10 +1,11 @@
 const app = require('./app');
+const env = require('./env');
 
 const { log } = console;
-const { PORT = 3759 } = process.env;
+const { INTERNAL_PORT, EXTERNAL_PORT } = env;
 
 app.get('/', (req, res) => {
   res.json({ ping: 'pong' });
 });
 
-app.listen(PORT, () => log(`Listening on http://0.0.0.0:${PORT}`));
+app.listen(INTERNAL_PORT, () => log(`Listening on http://0.0.0.0:${EXTERNAL_PORT}`));
