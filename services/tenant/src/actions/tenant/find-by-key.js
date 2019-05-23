db8f35b986ec75b6821d0f608b81c8f28163c980
Jacob Bare <jacob@limit0.io>
2019-05-23 15:21:28 -0500

    Add tenant create action and query db on find

--- services/tenant/src/actions/tenant/find-by-key.js
+++ services/tenant/src/actions/tenant/find-by-key.js
@@ -1,8 +1,10 @@
 const { service } = require('@base-cms/micro');
+const Tenant = require('../../mongodb/models/tenant');
 
 const { createRequiredParamError } = service;
 
 module.exports = async ({ key }) => {
   if (!key) throw createRequiredParamError('key');
-  return { key };
+  const tenant = await Tenant.findOne({ key, deleted: { $ne: true } });
+  return tenant || null;
 };
--- services/tenant/src/actions/tenant/index.js
+++ services/tenant/src/actions/tenant/index.js
@@ -1,5 +1,7 @@
+const create = require('./create');
 const findByKey = require('./find-by-key');
 
 module.exports = {
+  create,
   findByKey,
 };
