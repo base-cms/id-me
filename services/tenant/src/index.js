fe4d2f163b2b3087f0136d7ef6dc7d13e9c9e009
Jacob Bare <jacob@limit0.io>
2019-05-23 14:05:58 -0500

    Add tenant service init

--- services/tenant/src/index.js
+++ services/tenant/src/index.js
@@ -1,8 +1,10 @@
 const { service } = require('@base-cms/micro');
 const actions = require('./actions');
+const init = require('./init');
 
 process.on('unhandledRejection', (e) => { throw e; });
 
 module.exports = service.json({
   actions,
+  init,
 });
