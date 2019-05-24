8e3d7d5dbcf053647332bb219356ea04352f8d3f
Jacob Bare <jacob@limit0.io>
2019-05-24 09:42:08 -0500

    Add user.findByEmail logic

--- services/organization/src/actions/user/find-by-email.js
+++ services/organization/src/actions/user/find-by-email.js
@@ -1,1 +1,10 @@
-module.exports = async () => ({});
+const { service } = require('@base-cms/micro');
+const User = require('../../mongodb/models/user');
+
+const { createRequiredParamError } = service;
+
+module.exports = async ({ email, fields }) => {
+  if (!email) throw createRequiredParamError('email');
+  const user = await User.findByEmail(email, fields);
+  return user || null;
+};
