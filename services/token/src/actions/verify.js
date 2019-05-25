1a07dbe71f87fb331288e68381435b94016f2fb9
Jacob Bare <jacob@limit0.io>
2019-05-25 09:02:50 -0500

    Add verify action

--- services/token/src/actions/verify.js
+++ services/token/src/actions/verify.js
@@ -1,1 +1,36 @@
-module.exports = async () => 'ok';
+const jwt = require('jsonwebtoken');
+const { createError } = require('micro');
+const { service } = require('@base-cms/micro');
+
+const { createRequiredParamError } = service;
+
+/**
+ * Verifies the provided encoded token.
+ *
+ * @param {object} params
+ * @param {string} params.action The corresponding action for the token.
+ * @param {string} params.encoded The encoded JWT value.
+ * @param {string} params.secret The secret used to sign the JWT.
+ */
+module.exports = async ({ action, encoded, secret }) => {
+  if (!action) throw createRequiredParamError('action');
+  if (!encoded) throw createRequiredParamError('encoded');
+  if (!secret) throw createRequiredParamError('secret');
+
+  try {
+    const verified = jwt.verify(encoded, secret, { algorithms: ['HS256'] });
+    return verified;
+  } catch (e) {
+    const { message } = e;
+    switch (e.message) {
+      case 'invalid signature':
+        throw createError(400, message);
+      case 'jwt expired':
+        throw createError(400, message);
+      case 'jwt malformed':
+        throw createError(422, message);
+      default:
+        throw e;
+    }
+  }
+};
