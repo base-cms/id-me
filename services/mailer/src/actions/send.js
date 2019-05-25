a119af8d18d085742cd4525bc6c7dfecc3eba001
Jacob Bare <jacob@limit0.io>
2019-05-25 09:59:34 -0500

    Add mailer send action

--- services/mailer/src/actions/send.js
+++ services/mailer/src/actions/send.js
@@ -1,1 +1,25 @@
-module.exports = async () => 'ok';
+const sgMail = require('@sendgrid/mail');
+const { service } = require('@base-cms/micro');
+const { SENDGRID_API_KEY } = require('../env');
+
+const { createRequiredParamError } = service;
+
+module.exports = async ({
+  to,
+  from,
+  subject,
+  html,
+}) => {
+  if (!to) throw createRequiredParamError('to');
+  if (!from) throw createRequiredParamError('from');
+  if (!subject) throw createRequiredParamError('subject');
+  if (!html) throw createRequiredParamError('html');
+
+  sgMail.setApiKey(SENDGRID_API_KEY);
+  return sgMail.send({
+    to,
+    from,
+    subject,
+    html,
+  });
+};
