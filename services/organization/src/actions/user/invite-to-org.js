0c61f085813d8f08c5e5ca73a5a045d8495bb4ad
Jacob Bare <jacob@limit0.io>
2019-05-25 08:24:52 -0500

    Properly upsert org invite

--- services/organization/src/actions/user/invite-to-org.js
+++ services/organization/src/actions/user/invite-to-org.js
@@ -15,20 +15,19 @@ module.exports = async ({
     email,
   });
 
-  const [org, user, membership] = await Promise.all([
+  const [org, membership] = await Promise.all([
     Organization.findById({ id: organizationId, fields: ['id', 'name'] }),
-    createUser({ email, fields: ['id'] }),
     OrgMembership.findOne({ email, organizationId }, { _id: 1 }),
+    createUser({ email, fields: ['id'] }),
   ]);
-  console.log(membership);
   if (!org) throw createError(404, `No organization was found for '${organizationId}'`);
   if (membership) throw createError(400, `This user is already a member of ${org.name}.`);
 
   const invite = new OrgInvitation({ email, organizationId, role });
   await invite.validate();
-  delete invite.id;
-  delete invite._id;
+  const $set = invite.toObject();
+  delete $set._id;
 
-  await OrgInvitation.update({ email, organizationId }, { $set: invite }, { upsert: true });
+  await OrgInvitation.update({ email, organizationId }, { $set }, { upsert: true });
   return 'ok';
 };
