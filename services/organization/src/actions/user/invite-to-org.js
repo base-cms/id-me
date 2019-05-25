f33cef7131cb1bf1cfaefc24954c1c8ed2acbafd
Jacob Bare <jacob@limit0.io>
2019-05-25 10:17:54 -0500

    Create initial invitation token and email

--- services/organization/src/actions/user/invite-to-org.js
+++ services/organization/src/actions/user/invite-to-org.js
@@ -1,4 +1,6 @@
 const { createError } = require('micro');
+const tokenService = require('@base-cms/id-me-token-client');
+const mailerService = require('@base-cms/id-me-mailer-client');
 const Organization = require('../organization');
 const createUser = require('./create');
 const validateParams = require('../validate-required-params');
@@ -15,10 +17,10 @@ module.exports = async ({
     email,
   });
 
-  const [org, membership] = await Promise.all([
+  const [org, membership, user] = await Promise.all([
     Organization.findById({ id: organizationId, fields: ['id', 'name'] }),
     OrgMembership.findOne({ email, organizationId }, { _id: 1 }),
-    createUser({ email, fields: ['id'] }),
+    createUser({ email, fields: ['id', 'email'] }),
   ]);
   if (!org) throw createError(404, `No organization was found for '${organizationId}'`);
   if (membership) throw createError(400, `This user is already a member of ${org.name}.`);
@@ -28,6 +30,31 @@ module.exports = async ({
   const $set = invite.toObject();
   delete $set._id;
 
-  await OrgInvitation.update({ email, organizationId }, { $set }, { upsert: true });
+  const payload = {
+    sub: 'invite-user-to-org',
+    oid: organizationId,
+    email: user.email,
+  };
+  const { token } = await tokenService.request('create', { params: { payload } });
+
+  await OrgInvitation.update({ email: user.email, organizationId }, { $set }, { upsert: true });
+
+  const html = `
+    <html>
+      <body>
+        <h1>You've been invited to join an organization on ID|Me.</h1>
+        <h2>${org.name}</h2>
+        <p><a href="http://www.google.com/join/${token}">Join the ${org.name} organization.</a></p>
+      </body>
+    </html>
+  `;
+  await mailerService.request('send', {
+    params: {
+      to: user.email,
+      from: 'ID|Me Platform <noreply@base-cms.io>',
+      subject: `You've been invited to join ${org.name}`,
+      html,
+    },
+  });
   return 'ok';
 };
