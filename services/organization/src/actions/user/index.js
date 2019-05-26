c328e370eb686e365ae3702ed9d35e255b090c8d
Jacob Bare <jacob@limit0.io>
2019-05-26 07:46:04 -0500

    Add user.acceptOrgInvite action

--- services/organization/src/actions/user/index.js
+++ services/organization/src/actions/user/index.js
@@ -1,8 +1,10 @@
+const acceptOrgInvite = require('./accept-org-invite');
 const create = require('./create');
 const findByEmail = require('./find-by-email');
 const inviteToOrg = require('./invite-to-org');
 
 module.exports = {
+  acceptOrgInvite,
   create,
   findByEmail,
   inviteToOrg,
