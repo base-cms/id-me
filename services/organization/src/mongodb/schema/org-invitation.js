04c8bb56600461c11054950baf9245505bea8942
Jacob Bare <jacob@limit0.io>
2019-05-28 07:22:47 -0500

    Update org invitation and membership email plugin

--- services/organization/src/mongodb/schema/org-invitation.js
+++ services/organization/src/mongodb/schema/org-invitation.js
@@ -1,11 +1,12 @@
 const { Schema } = require('mongoose');
+const connection = require('../connection');
 const emailPlugin = require('./shared/email-plugin');
 const orgRolePlugin = require('./shared/org-role-plugin');
 const organizationPlugin = require('./shared/organization-plugin');
 
 const schema = new Schema({}, { timestamps: true });
 
-schema.plugin(emailPlugin);
+schema.plugin(emailPlugin, { connection });
 schema.plugin(orgRolePlugin);
 schema.plugin(organizationPlugin);
 
