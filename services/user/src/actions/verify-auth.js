d132f6b195f399a4ee085c785a64419d6087b7f3
Jacob Bare <jacob@limit0.io>
2019-05-28 14:06:54 -0500

    Wrap auth verification and throw 401

--- services/user/src/actions/verify-auth.js
+++ services/user/src/actions/verify-auth.js
@@ -5,12 +5,16 @@ const findByEmail = require('./find-by-email');
 
 module.exports = async ({ token } = {}) => {
   if (!token) throw createRequiredParamError('token');
+  try {
+    const verified = await tokenService.request('verify', { sub: 'user-auth', token });
+    const { aud: email } = verified;
+    if (!email) throw createError(400, 'No email address was provided in the token payload');
 
-  const { aud: email } = await tokenService.request('verify', { sub: 'user-auth', token });
-  if (!email) throw createError(400, 'No email address was provided in the token payload');
+    const user = await findByEmail({ email, fields: ['id', 'email'] });
+    if (!user) throw createError(404, `No user was found for '${email}'`);
 
-  const user = await findByEmail({ email, fields: ['id', 'email'] });
-  if (!user) throw createError(404, `No user was found for '${email}'`);
-
-  return 'ok';
+    return verified;
+  } catch (e) {
+    throw createError(401, e.message);
+  }
 };
