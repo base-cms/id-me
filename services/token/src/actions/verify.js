a5a26df88b028f54585baf7a6abddc2c05ec3fd6
Jacob Bare <jacob@limit0.io>
2019-05-28 13:34:25 -0500

    Ensure token exists in db and subjects match

--- services/token/src/actions/verify.js
+++ services/token/src/actions/verify.js
@@ -1,6 +1,7 @@
 const jwt = require('jsonwebtoken');
 const { createError } = require('micro');
 const { service } = require('@base-cms/micro');
+const Token = require('../mongodb/models/token');
 const { TOKEN_SECRET } = require('../env');
 
 const { createRequiredParamError } = service;
@@ -10,12 +11,20 @@ const { createRequiredParamError } = service;
  *
  * @param {object} params
  * @param {string} params.token The encoded JWT value.
+ * @param {string} params.sub The token subject.
  */
-module.exports = async ({ token }) => {
+module.exports = async ({ token, sub }) => {
   if (!token) throw createRequiredParamError('token');
+  if (!sub) throw createRequiredParamError('sub');
 
   try {
+    // Verify the token signature.
     const verified = jwt.verify(token, TOKEN_SECRET, { algorithms: ['HS256'] });
+    // Ensure the token exists in the db and matches the subject.
+    const { jti } = verified;
+    const doc = await Token.findById(jti);
+    if (!doc) throw createError(400, 'No token was found for the provided token identifier.');
+    if (sub !== doc.sub) throw createError(400, 'The token subject does not match.');
     return verified;
   } catch (e) {
     const { message } = e;
