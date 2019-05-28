95d4ea9a2855cdf5248cf36f3a2671d204c439ea
Jacob Bare <jacob@limit0.io>
2019-05-28 07:22:33 -0500

    Use email plugin on user model

--- services/organization/src/mongodb/schema/user.js
+++ services/organization/src/mongodb/schema/user.js
@@ -1,23 +1,8 @@
-const validator = require('validator');
 const { Schema } = require('mongoose');
+const emailPlugin = require('./shared/email-plugin');
 const connection = require('../connection');
 
 const schema = new Schema({
-  email: {
-    type: String,
-    required: true,
-    trim: true,
-    unique: true,
-    lowercase: true,
-    validate: [
-      {
-        validator(email) {
-          return validator.isEmail(email);
-        },
-        message: 'Invalid email address {VALUE}',
-      },
-    ],
-  },
   givenName: {
     type: String,
     trim: true,
@@ -28,15 +13,9 @@ const schema = new Schema({
   },
 }, { timestamps: true });
 
-schema.static('normalizeEmail', (email) => {
-  if (!email) return '';
-  return String(email).trim().toLowerCase();
-});
-
-schema.static('findByEmail', async function findByEmail(value, fields) {
-  const email = connection.model('user').normalizeEmail(value);
-  if (!email) throw new Error('Unable to find user: no email address was provided.');
-  return this.findOne({ email }, fields);
+schema.plugin(emailPlugin, {
+  connection,
+  fieldOptions: { unique: true },
 });
 
 module.exports = schema;
