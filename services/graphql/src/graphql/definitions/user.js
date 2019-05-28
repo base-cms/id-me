853b1ad2608fa4e9c2598d2e97c37da41cd638cb
Jacob Bare <jacob@limit0.io>
2019-05-28 16:11:15 -0500

    Add userLogin and sendUserLoginLink

--- services/graphql/src/graphql/definitions/user.js
+++ services/graphql/src/graphql/definitions/user.js
@@ -4,11 +4,35 @@ module.exports = gql`
 
 extend type Mutation {
   inviteUserToOrg(input: InviteUserToOrgMutationInput!): String
+  sendUserLoginLink(input: SendUserLoginLinkMutationInput!): String
+  userLogin(input: UserLoginMutationInput!): String
+}
+
+type UserAuthentication {
+  user: User!
+  token: UserAuthToken!
+}
+
+type User {
+  id: String!
+  email: String!
+}
+
+type UserAuthToken {
+  id: String!
 }
 
 input InviteUserToOrgMutationInput {
   email: String!
   role: OrganizationRole = Member
+}
+
+input SendUserLoginLinkMutationInput {
+  email: String!
+}
+
+input UserLoginMutationInput {
+  token: String!
 }
 
 `;
--- services/graphql/src/graphql/resolvers/user.js
+++ services/graphql/src/graphql/resolvers/user.js
@@ -1,7 +1,22 @@
 const userService = require('@base-cms/id-me-user-client');
 
 module.exports = {
+  User: {
+    id: user => user._id,
+  },
+
   Mutation: {
+    sendUserLoginLink: (_, { input }) => {
+      const { email } = input;
+      return userService.request('sendLoginLink', { email });
+    },
+
+    userLogin: async (_, { input }) => {
+      const { token } = input;
+      const { authToken } = await userService.request('login', { token });
+      return authToken;
+    },
+
     inviteUserToOrg: (_, { input }, { org }) => {
       org.check();
       // @todo Must determine if user is logged in.
