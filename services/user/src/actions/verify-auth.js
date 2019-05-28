d2dd43cb2dcb7ca2980c6a1bb720328b45d22903
Jacob Bare <jacob@limit0.io>
2019-05-28 16:11:00 -0500

    Return user and decoded token; ensure emails match

--- services/user/src/actions/verify-auth.js
+++ services/user/src/actions/verify-auth.js
@@ -3,17 +3,19 @@ const { createError } = require('micro');
 const { createRequiredParamError } = require('@base-cms/micro').service;
 const findByEmail = require('./find-by-email');
 
-module.exports = async ({ token } = {}) => {
+module.exports = async ({ token, fields } = {}) => {
   if (!token) throw createRequiredParamError('token');
   try {
     const verified = await tokenService.request('verify', { sub: 'user-auth', token });
     const { aud: email } = verified;
     if (!email) throw createError(400, 'No email address was provided in the token payload');
 
-    const user = await findByEmail({ email, fields: ['id', 'email'] });
+    const user = await findByEmail({ email, fields });
     if (!user) throw createError(404, `No user was found for '${email}'`);
+    console.log(email, user.email);
+    if (user.email !== email) throw createError(401, 'The user email does not match the token email.');
 
-    return verified;
+    return { user, token: verified };
   } catch (e) {
     throw createError(401, e.message);
   }
