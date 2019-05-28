9786914889e84e8ae9ae4271a575ad708837937f
Jacob Bare <jacob@limit0.io>
2019-05-28 07:22:08 -0500

    Move static methods to email plugin

--- services/organization/src/mongodb/schema/shared/email-plugin.js
+++ services/organization/src/mongodb/schema/shared/email-plugin.js
@@ -1,14 +1,23 @@
+const { normalizeEmail } = require('@base-cms/id-me-utils');
 const emailValidator = require('./email-validator');
 
-module.exports = function emailPlugin(schema, options = {}) {
+module.exports = function emailPlugin(schema, { connection, fieldOptions = {} } = {}) {
   schema.add({
     email: {
-      ...options,
+      ...fieldOptions,
       type: String,
       required: true,
       trim: true,
       lowercase: true,
       validate: emailValidator,
     },
+  });
+
+  schema.static('normalizeEmail', normalizeEmail);
+
+  schema.static('findByEmail', async function findByEmail(value, fields) {
+    const email = connection.model('user').normalizeEmail(value);
+    if (!email) throw new Error('Unable to find user: no email address was provided.');
+    return this.findOne({ email }, fields);
   });
 };
