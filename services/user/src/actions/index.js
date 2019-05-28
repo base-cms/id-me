6583ea824ffd32316723032d986f0dde90b243bf
Jacob Bare <jacob@limit0.io>
2019-05-28 12:34:52 -0500

    Create user.sendLogin action

--- services/user/src/actions/index.js
+++ services/user/src/actions/index.js
@@ -2,10 +2,12 @@ const acceptOrgInvite = require('./accept-org-invite');
 const create = require('./create');
 const findByEmail = require('./find-by-email');
 const inviteToOrg = require('./invite-to-org');
+const sendLogin = require('./send-login');
 
 module.exports = {
   acceptOrgInvite,
   create,
   findByEmail,
   inviteToOrg,
+  sendLogin,
 };
---
+++ services/user/src/actions/send-login.js
@@ -0,0 +1,36 @@
+const mailerService = require('@base-cms/id-me-mailer-client');
+const tokenService = require('@base-cms/id-me-token-client');
+const { createError } = require('micro');
+const { createRequiredParamError } = require('@base-cms/micro').service;
+const findByEmail = require('./find-by-email');
+
+module.exports = async ({ email } = {}) => {
+  if (!email) throw createRequiredParamError('email');
+
+  const user = await findByEmail({ email, fields: ['id', 'email'] });
+
+  if (!user) throw createError(404, `No user was found for '${email}'`);
+
+  const payload = {
+    sub: 'user-login',
+    email: user.email,
+  };
+  const { token } = await tokenService.request('create', { payload, ttl: 60 * 60 });
+
+  const html = `
+    <html>
+      <body>
+        <h1>You're personal login link.</h1>
+        <p>The login link is good for one hour. If you did not request this link, simply ignore this email or contact support.</p>
+        <p><a href="http://www.google.com/login/${token}">Login to ID|Me</a></p>
+      </body>
+    </html>
+  `;
+  await mailerService.request('send', {
+    to: user.email,
+    from: 'ID|Me Platform <noreply@base-cms.io>',
+    subject: 'You\'re personal login link',
+    html,
+  });
+  return 'ok';
+};
