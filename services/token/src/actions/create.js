c338f2198c1291314902c0441e52e93923afff42
Jacob Bare <jacob@limit0.io>
2019-05-28 13:28:56 -0500

    Insert token to database on create

--- services/token/src/actions/create.js
+++ services/token/src/actions/create.js
@@ -1,11 +1,16 @@
 const jwt = require('jsonwebtoken');
 const uuid = require('uuid/v4');
+const { createRequiredParamError } = require('@base-cms/micro').service;
 const { TOKEN_SECRET } = require('../env');
+const Token = require('../mongodb/models/token');
 
 /**
  * Creates an encoded JWT for the provided payload.
- * If the `jti` and `iat` values are not present in the payload, they will automatically be added.
  *
+ * The `sub` param is required and cannot be overridden by the payload object.
+ * The `jti` value is auto-generated, and cannot be overriden by the payload object.
+ *
+ * If the `iat` value is not present in the payload, it will automatically be added.
  * If an `exp` values is provided in the payload, it will override the `ttl` argument.
  *
  * @param {object} params
@@ -15,18 +20,22 @@ const { TOKEN_SECRET } = require('../env');
 module.exports = async ({
   payload = {},
   ttl,
+  sub,
 } = {}) => {
+  if (!sub) throw createRequiredParamError('sub');
   const now = new Date();
   const iat = Math.floor(now.valueOf() / 1000);
 
+  const jti = uuid();
   const exp = ttl ? iat + ttl : undefined;
-  const initial = { jti: uuid(), iat };
+  const initial = { jti, iat, sub };
   if (exp) initial.exp = exp;
 
   const toSign = {
-    ...initial,
     ...payload,
+    ...initial,
   };
   const token = jwt.sign(toSign, TOKEN_SECRET);
+  await Token.create({ _id: jti, sub, payload: toSign });
   return { token, payload: toSign };
 };
