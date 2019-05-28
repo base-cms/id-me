014cafc9645060e25892f3981e77df20d9f3dc9d
Jacob Bare <jacob@limit0.io>
2019-05-28 07:47:44 -0500

    Remove outdated context calls

--- services/graphql/src/graphql/context.js
+++ services/graphql/src/graphql/context.js
@@ -1,10 +1,1 @@
-const { UserInputError } = require('apollo-server-express');
-const Organization = require('../mongodb/models/organization');
-
-module.exports = async ({ req }) => {
-  const organizationId = req.get('x-organization-id');
-  if (!organizationId) throw new UserInputError("The required 'x-organization-id' header was not provided.");
-  const organization = await Organization.findByIdActive(organizationId);
-  if (!organization) throw new UserInputError(`No organization was found for '${organizationId}'`);
-  return { organization };
-};
+module.exports = async () => ({});
