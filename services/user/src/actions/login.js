995985841bac34fde93d353cdac99d3eb2964ef5
Jacob Bare <jacob@limit0.io>
2019-05-29 08:29:00 -0500

    Return user and token (with id) objects from login

--- services/user/src/actions/login.js
+++ services/user/src/actions/login.js
@@ -3,17 +3,17 @@ const { createError } = require('micro');
 const { createRequiredParamError } = require('@base-cms/micro').service;
 const findByEmail = require('./find-by-email');
 
-module.exports = async ({ token } = {}) => {
+module.exports = async ({ token, fields } = {}) => {
   if (!token) throw createRequiredParamError('token');
 
   const { aud: email, jti } = await tokenService.request('verify', { sub: 'user-login-link', token });
   if (!email) throw createError(400, 'No email address was provided in the token payload');
 
-  const user = await findByEmail({ email, fields: ['id', 'email'] });
+  const user = await findByEmail({ email, fields });
   if (!user) throw createError(404, `No user was found for '${email}'`);
 
   // Create the authenticated token.
-  const { token: authToken } = await tokenService.request('create', {
+  const { token: authToken, payload } = await tokenService.request('create', {
     sub: 'user-auth',
     payload: { aud: user.email },
   });
@@ -21,5 +21,5 @@ module.exports = async ({ token } = {}) => {
   // Invalidate the login link token.
   tokenService.request('invalidate', { jti });
 
-  return { ...user.toObject(), authToken };
+  return { user: user.toObject(), token: { id: payload.jti, value: authToken } };
 };
