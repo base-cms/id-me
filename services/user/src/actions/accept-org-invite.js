893c6970d938274ba14464474bc45ff7a16df3e7
Jacob Bare <jacob@limit0.io>
2019-05-29 09:45:28 -0500

    Defer org membership to action and require token

--- services/user/src/actions/accept-org-invite.js
+++ services/user/src/actions/accept-org-invite.js
@@ -1,34 +1,39 @@
-const orgService = require('@base-cms/id-me-organization-client');
+const tokenService = require('@base-cms/id-me-token-client');
 const { createError } = require('micro');
 const { createRequiredParamError } = require('@base-cms/micro').service;
-const findByEmail = require('./find-by-email');
+const setOrgMembership = require('./set-org-membership');
 const { OrgMembership, OrgInvitation } = require('../mongodb/models');
+const findByEmail = require('./find-by-email');
 
 module.exports = async ({
   organizationId,
   email,
+  token,
 }) => {
   if (!organizationId) throw createRequiredParamError('organizationId');
   if (!email) throw createRequiredParamError('email');
+  if (!token) throw createRequiredParamError('token');
 
-  const [org, membership, invite, user] = await Promise.all([
-    orgService.request('findById', { id: organizationId, fields: ['id', 'name'] }),
+  const [membership, invite, user] = await Promise.all([
     OrgMembership.findFor(organizationId, email, ['id']),
     OrgInvitation.findFor(organizationId, email, ['organizationId', 'email', 'role']),
-    findByEmail({ email, fields: ['email'] }),
+    findByEmail({ email, fields: ['id', 'email'] }),
   ]);
 
-  if (!org) throw createError(404, `No organization was found for '${organizationId}'`);
-  if (!user) throw createError(404, `No user was found for '${email}'`);
-  if (!invite) throw createError(404, `No invitation to ${org.name} was found for ${email}.`);
-  if (membership) throw createError(400, `This user is already a member of ${org.name}.`);
+  if (!user) throw createError(404, `No user was found for ${email}`);
+  if (!invite) throw createError(404, `No invitation to this organization was found for ${email}.`);
+  if (membership) throw createError(400, 'This user is already a member of this organization.');
 
-  const member = new OrgMembership({
-    organizationId,
-    email: user.email,
-    role: invite.role,
-  });
-  await member.save();
-  await OrgInvitation.remove({ email: user.email, organizationId });
+  const { aud, jti } = await tokenService.request('verify', { sub: 'invite-user-to-org', token });
+  if (!aud) throw createError(400, 'No email address was provided in the token payload');
+  if (user.email !== aud) throw createError(403, 'The user email does not match the token email.');
+
+  await setOrgMembership({ organizationId, email, role: invite.role });
+
+  // Remove the invitation (but do not await)
+  OrgInvitation.remove({ email: user.email, organizationId });
+
+  // Invalidate the login link token (but do not await)
+  tokenService.request('invalidate', { jti });
   return 'ok';
 };
