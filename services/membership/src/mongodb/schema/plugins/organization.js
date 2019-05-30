6945862171eb2d05293d5e5432799610e664625b
Jacob Bare <jacob@limit0.io>
2019-05-30 11:59:01 -0500

    Use normalizeEmail directly

--- services/membership/src/mongodb/schema/plugins/organization.js
+++ services/membership/src/mongodb/schema/plugins/organization.js
@@ -1,5 +1,5 @@
 const { referencePlugin } = require('@base-cms/id-me-mongoose-plugins');
-const connection = require('../../connection');
+const { normalizeEmail } = require('@base-cms/id-me-utils');
 
 module.exports = function organizationPlugin(schema) {
   schema.plugin(referencePlugin, {
@@ -9,21 +9,21 @@ module.exports = function organizationPlugin(schema) {
   schema.index({ organizationId: 1, email: 1 }, { unique: true });
 
   schema.static('findFor', async function findFor(organizationId, emailAddress, fields) {
-    const email = connection.model('user').normalizeEmail(emailAddress);
+    const email = normalizeEmail(emailAddress);
     if (!email) throw new Error('Unable to find: no email address was provided.');
     if (!organizationId) throw new Error('Unable to find: no organization ID was provided.');
     return this.findOne({ organizationId, email }, fields);
   });
 
   schema.static('deleteFor', async function deleteFor(organizationId, emailAddress) {
-    const email = connection.model('user').normalizeEmail(emailAddress);
+    const email = normalizeEmail(emailAddress);
     if (!email) throw new Error('Unable to remove: no email address was provided.');
     if (!organizationId) throw new Error('Unable to remove: no organization ID was provided.');
     return this.deleteOne({ organizationId, email });
   });
 
   schema.static('findForUser', async function findForUser(emailAddress, fields) {
-    const email = connection.model('user').normalizeEmail(emailAddress);
+    const email = normalizeEmail(emailAddress);
     if (!email) throw new Error('Unable to find: no email address was provided.');
     return this.find({ email }, fields);
   });
