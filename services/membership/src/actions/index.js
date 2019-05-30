43ff2db0c12e34a6569a1b7a275f2ed642a00377
Jacob Bare <jacob@limit0.io>
2019-05-30 09:16:07 -0500

    Add membership.changeRole action

--- services/membership/src/actions/index.js
+++ services/membership/src/actions/index.js
@@ -1,10 +1,12 @@
 const acceptInvite = require('./accept-invite');
+const changeRole = require('./change-role');
 const create = require('./create');
 const deleteInvite = require('./delete-invite');
 const invite = require('./invite');
 
 module.exports = {
   acceptInvite,
+  changeRole,
   create,
   deleteInvite,
   invite,
