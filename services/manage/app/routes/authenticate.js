6f8bcc45f4b5593a2e6a5106b5f0ebecddb71fe8
Josh Worden <solocommand@gmail.com>
2019-05-30 14:22:42 -0500

    Set the attempted transition to handle login redirect

--- services/manage/app/routes/authenticate.js
+++ services/manage/app/routes/authenticate.js
@@ -5,13 +5,12 @@ import { inject as service } from '@ember/service';
 
 export default Route.extend(UnauthenticatedRouteMixin, {
   routeIfAlreadyAuthenticated: 'manage',
-  routeAfterAuthentication: 'manage',
 
   session: service(),
 
   async model({ token }, transition) {
     const route = get(transition, 'to.queryParams.route');
-    if (route) this.set('routeAfterAuthentication', route);
+    if (route) this.session.set('attemptedTransition', this.transitionTo(route));
     return this.session.authenticate('authenticator:application', token);
   },
 });
