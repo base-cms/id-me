8b4f2effb51e0d252a3778a543ca72ad3df4cb3d
Jacob Bare <jacob@limit0.io>
2019-05-30 13:21:20 -0500

    Add application findById and updateField actions

--- services/application/src/actions/index.js
+++ services/application/src/actions/index.js
@@ -1,5 +1,9 @@
 const create = require('./create');
+const findById = require('./find-by-id');
+const updateField = require('./update-field');
 
 module.exports = {
   create,
+  findById,
+  updateField,
 };
