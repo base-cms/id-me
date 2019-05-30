ace3d7b0277e3d05e05852c1efee342dc9e91bd1
Jacob Bare <jacob@limit0.io>
2019-05-30 11:59:37 -0500

    Remove individual service client packages

--- packages/mailer-client/src/index.js
+++
@@ -1,4 +0,0 @@
-const { client } = require('@base-cms/micro');
-const { MAILER_SERVICE_URL } = require('./env');
-
-module.exports = client.json({ url: MAILER_SERVICE_URL });
--- packages/membership-client/src/index.js
+++
@@ -1,4 +0,0 @@
-const { client } = require('@base-cms/micro');
-const { MEMBERSHIP_SERVICE_URL } = require('./env');
-
-module.exports = client.json({ url: MEMBERSHIP_SERVICE_URL });
