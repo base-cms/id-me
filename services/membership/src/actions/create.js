d3065b029420fa4e4f5d2955a0b99918ef1aaf8e
Jacob Bare <jacob@limit0.io>
2019-05-30 09:10:28 -0500

    Add membership.create action

---
+++ services/membership/src/actions/create.js
@@ -0,0 +1,38 @@
+const orgService = require('@base-cms/id-me-organization-client');
+const userService = require('@base-cms/id-me-user-client');
+const { createError } = require('micro');
+const { createRequiredParamError } = require('@base-cms/micro').service;
+
+const { OrgMembership } = require('../mongodb/models');
+
+const createMembership = async ({ email, organizationId, role }) => {
+  const member = new OrgMembership({ email, organizationId, role });
+  await member.validate();
+  const obj = member.toObject();
+  delete obj._id;
+  return obj;
+};
+
+module.exports = async ({
+  organizationId,
+  email,
+  role,
+}) => {
+  if (!organizationId) throw createRequiredParamError('organizationId');
+  if (!email) throw createRequiredParamError('email');
+  if (!role) throw createRequiredParamError('role');
+
+  const [org, user] = await Promise.all([
+    orgService.request('findById', { id: organizationId, fields: ['id'] }),
+    userService.request('findByEmail', { email, fields: ['email'] }),
+  ]);
+
+  if (!org) throw createError(404, `No organization was found for '${organizationId}'`);
+  if (!user) throw createError(404, `No user was found for '${email}'`);
+
+  const query = { email: user.email, organizationId };
+  const member = await createMembership({ ...query, role });
+
+  await OrgMembership.updateOne(query, { $setOnInsert: member }, { upsert: true });
+  return 'ok';
+};
--- services/membership/src/actions/index.js
+++ services/membership/src/actions/index.js
@@ -1,5 +1,7 @@
+const create = require('./create');
 const invite = require('./invite');
 
 module.exports = {
+  create,
   invite,
 };
