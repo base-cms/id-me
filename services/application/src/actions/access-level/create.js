7004761d976f7a5916b071fea2d4d90f825bcb94
Jacob Bare <jacob@limit0.io>
2019-05-31 09:35:34 -0500

    Add application.access-level.create action

---
+++ services/application/src/actions/access-level/create.js
@@ -0,0 +1,24 @@
+const { createError } = require('micro');
+const { createRequiredParamError } = require('@base-cms/micro').service;
+const { handleError } = require('@base-cms/id-me-utils').mongoose;
+const { isObject } = require('@base-cms/utils');
+
+const { Application, AccessLevel } = require('../../mongodb/models');
+
+module.exports = async ({ applicationId, payload } = {}) => {
+  if (!applicationId) throw createRequiredParamError('applicationId');
+  if (!isObject(payload)) throw createRequiredParamError('payload');
+
+  const application = await Application.findById(applicationId, ['id', 'name']);
+  if (!application) throw createError(404, `No application was found for '${applicationId}'`);
+
+  try {
+    const level = await AccessLevel.create({
+      ...payload,
+      application,
+    });
+    return level;
+  } catch (e) {
+    throw handleError(e);
+  }
+};
--- services/application/src/actions/index.js
+++ services/application/src/actions/index.js
@@ -1,10 +1,19 @@
+const accessLevel = require('./access-level');
 const create = require('./create');
 const findById = require('./find-by-id');
 const listForOrg = require('./list-for-org');
 const updateField = require('./update-field');
 const updateMany = require('./update-many');
 
+const { keys } = Object;
+
+const load = (root, obj) => keys(obj).reduce((o, key) => {
+  const k = `${root}.${key}`;
+  return { ...o, [k]: obj[key] };
+}, {});
+
 module.exports = {
+  ...load('access-level', accessLevel),
   create,
   findById,
   listForOrg,
