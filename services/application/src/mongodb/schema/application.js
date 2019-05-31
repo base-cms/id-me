ef19d81604c506bb5ae97d2d4ae45a1c7e783f9d
Jacob Bare <jacob@limit0.io>
2019-05-31 10:17:46 -0500

    Use org plugin on app schema

--- services/application/src/mongodb/schema/application.js
+++ services/application/src/mongodb/schema/application.js
@@ -1,12 +1,5 @@
 const { Schema } = require('mongoose');
-
-const organizationSchema = new Schema({
-  name: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-});
+const { organizationPlugin } = require('@base-cms/id-me-mongoose-plugins');
 
 const schema = new Schema({
   name: {
@@ -18,17 +11,8 @@ const schema = new Schema({
     type: String,
     trim: true,
   },
-  organization: {
-    type: organizationSchema,
-    required: true,
-  },
 }, { timestamps: true });
 
-schema.index({ 'organization._id': 1 });
-
-schema.static('findForOrganization', async function findForOrganization(organizationId, fields) {
-  if (!organizationId) throw new Error('Unable to find: no organization ID was provided.');
-  return this.find({ 'organization._id': organizationId }, fields);
-});
+schema.plugin(organizationPlugin);
 
 module.exports = schema;
