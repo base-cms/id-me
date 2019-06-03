76323eccb066d156204da64284440c4c28ce897e
Josh Worden <solocommand@gmail.com>
2019-06-03 14:26:52 -0500

    Skip NR deploy for non-configured apps

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -30,7 +30,7 @@ const image = `basecms/identity-x-${service}-service`;
 
 const nrIds = {
   application: 238098240,
-  grapqhl: 238106643,
+  graphql: 238106643,
   mailer: 238106582,
   membership: 238106610,
   organization: 238106397,
