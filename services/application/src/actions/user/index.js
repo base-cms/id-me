e60469ac189e3d800adb4e3675a3099116098372
Jacob Bare <jacob@limit0.io>
2019-06-03 16:01:33 -0500

    Add user.updateField action

--- services/application/src/actions/user/index.js
+++ services/application/src/actions/user/index.js
@@ -1,9 +1,13 @@
+const { updateField } = require('@base-cms/id-me-utils').actions;
 const create = require('./create');
 const findByEmail = require('./find-by-email');
 const sendLoginLink = require('./send-login-link');
+
+const AppUser = require('../../mongodb/models/app-user');
 
 module.exports = {
   create,
   findByEmail,
   sendLoginLink,
+  updateField: params => updateField(AppUser, params),
 };
