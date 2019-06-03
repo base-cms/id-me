cb4076003fb560201d3a236c697c156e77d2ab4c
Josh Worden <solocommand@gmail.com>
2019-06-03 13:56:27 -0500

    Support tracing via Apollo Engine and New Relic

--- services/graphql/src/env.js
+++ services/graphql/src/env.js
@@ -8,6 +8,7 @@ const {
 const { nonemptystr } = validators;
 
 module.exports = cleanEnv(process.env, {
+  ENGINE_API_KEY: nonemptystr({ desc: 'The Apollo Engine API key', devDefault: '(unset)' }),
   EXTERNAL_PORT: port({ desc: 'The external port that express is exposed on.', default: 80 }),
   INTERNAL_PORT: port({ desc: 'The internal port that express will run on.', default: 80 }),
   MONGO_DSN: nonemptystr({ desc: 'The MongoDB DSN to connect to.' }),
