979c8e6fc1cd91b2e8dfaa1daf23d578130ec70d
Jacob Bare <jacob@limit0.io>
2019-06-04 13:07:20 -0500

    Add application.user.verifyAuth action

--- services/application/src/actions/user/index.js
+++ services/application/src/actions/user/index.js
@@ -4,6 +4,7 @@ const findByEmail = require('./find-by-email');
 const login = require('./login');
 const logout = require('./logout');
 const sendLoginLink = require('./send-login-link');
+const verifyAuth = require('./verify-auth');
 
 const AppUser = require('../../mongodb/models/app-user');
 
@@ -14,4 +15,5 @@ module.exports = {
   logout,
   sendLoginLink,
   updateField: params => updateField(AppUser, params),
+  verifyAuth,
 };
---
+++ services/application/src/actions/user/verify-auth.js
@@ -0,0 +1,28 @@
+const { createError } = require('micro');
+const { createRequiredParamError } = require('@base-cms/micro').service;
+const { tokenService } = require('@base-cms/id-me-service-clients');
+const { Application } = require('../../mongodb/models');
+
+const findByEmail = require('./find-by-email');
+
+module.exports = async ({ token, fields } = {}) => {
+  if (!token) throw createRequiredParamError('token');
+
+  try {
+    const verified = await tokenService.request('verify', { sub: 'app-user-auth', token });
+    const { aud: email, iss: applicationId } = verified;
+    if (!email) throw createError(400, 'No email address was provided in the token payload');
+    if (!applicationId) throw createError(400, 'No application ID was provided in the token payload');
+
+    const app = await Application.findById(applicationId, ['id']);
+    if (!app) throw createError(404, `No application was found for '${applicationId}'`);
+
+    const user = await findByEmail({ applicationId, email, fields });
+    if (!user) throw createError(404, `No user was found for '${email}'`);
+    if (user.email !== email) throw createError(401, 'The user email does not match the token email.');
+
+    return { user, token: verified };
+  } catch (e) {
+    throw createError(401, e.message);
+  }
+};
