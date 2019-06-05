bb0c22e061eb67f80b11be472daaa1ec894b0b65
Jacob Bare <jacob@limit0.io>
2019-06-05 08:42:42 -0500

    Add todos about app-id matching

--- services/application/src/mongodb/schema/plugins/access-level.js
+++ services/application/src/mongodb/schema/plugins/access-level.js
@@ -2,6 +2,9 @@ const { Schema } = require('mongoose');
 
 module.exports = function accessLevelPlugin(schema) {
   schema.add({
+    /**
+     * @todo Must ensure that the access level app id matches the parent app id.
+     */
     accessLevelIds: {
       type: [Schema.Types.ObjectId],
       index: true,
--- services/application/src/mongodb/schema/plugins/team.js
+++ services/application/src/mongodb/schema/plugins/team.js
@@ -2,6 +2,9 @@ const { Schema } = require('mongoose');
 
 module.exports = function teamPlugin(schema) {
   schema.add({
+    /**
+     * @todo Must ensure that the team app id matches the parent app id.
+     */
     teamIds: {
       type: [Schema.Types.ObjectId],
       index: true,
