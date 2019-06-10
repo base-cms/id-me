e01c069147b6c8532e84647e03b6939ff52d660d
Josh Worden <solocommand@gmail.com>
2019-06-10 14:40:54 -0500

    Restructure navs

--- services/manage/app/components/nav-bar.js
+++ services/manage/app/components/nav-bar.js
@@ -3,8 +3,7 @@ import { computed } from '@ember/object';
 import { inject as service } from '@ember/service';
 
 export default Component.extend({
-  tagName: 'nav',
-  classNames: ['navbar', 'navbar-expand', 'navbar-dark', 'bg-primary'],
+  tagName: 'header',
 
   router: service(),
 
@@ -12,11 +11,22 @@ export default Component.extend({
     const { currentURL, currentRouteName } = this.router;
     if (currentRouteName.includes('apps.app')) return currentURL.match(/apps\/([a-f0-9]{24})/i)[1];
   }),
+  application: computed('appId,applications.[]', function() {
+    const appId = this.get('appId');
+    const apps = this.get('applications') || [];
+    return apps.filter(({ id }) => id === appId).reduce((o, v) => v, null);
+  }),
 
   orgId: computed('router.{currentRouteName,currentURL}', function() {
     const { currentURL, currentRouteName } = this.router;
     if (currentRouteName.includes('orgs.org')) return currentURL.match(/orgs\/([a-f0-9]{24})/i)[1];
   }),
+  organization: computed('orgId,organizations.[]', function() {
+    const orgId = this.get('orgId');
+    const orgs = this.get('organizations') || [];
+    return orgs.filter(({ id }) => id === orgId).reduce((o, v) => v, null);
+  }),
 
   organizations: computed.mapBy('userOrganizations', 'organization'),
+  applications: computed.reads('organization.applications'),
 });
---
+++ services/manage/app/components/nav-context-app.js
@@ -0,0 +1,11 @@
+import Component from '@ember/component';
+import { computed } from '@ember/object';
+import { inject as service } from '@ember/service';
+
+export default Component.extend({
+  tagName: null,
+
+  user: service(),
+  show: computed.reads('user.isAuthenticated'),
+
+});
