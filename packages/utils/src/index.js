97b769900f285ea8b94e14d7288a74e3c43b788f
Josh Worden <solocommand@gmail.com>
2019-06-13 12:08:03 -0500

    remove ip6 from utils package

--- packages/utils/src/index.js
+++ packages/utils/src/index.js
@@ -1,13 +1,11 @@
 const actions = require('./actions');
 const createLoginToken = require('./create-login-token');
-const ip6 = require('./ip6');
 const mongoose = require('./mongoose');
 const normalizeEmail = require('./nomalize-email');
 
 module.exports = {
   actions,
   createLoginToken,
-  ip6,
   mongoose,
   normalizeEmail,
 };
