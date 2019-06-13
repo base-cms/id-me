e178a6a1c65a0cf13b8e47acf5bad52ea3bf8482
Josh Worden <solocommand@gmail.com>
2019-06-13 08:16:54 -0500

    Replace IP Validator with CIDR validator

--- services/application/src/mongodb/schema/team.js
+++ services/application/src/mongodb/schema/team.js
@@ -1,10 +1,37 @@
-const { Schema } = require('mongoose');
+const { Schema, Decimal128 } = require('mongoose');
+const { parse, createCIDR } = require('ip6addr');
 const {
   domainValidator,
-  ipValidator,
+  cidrValidator,
   applicationPlugin,
 } = require('@base-cms/id-me-mongoose-plugins');
 const accessLevelPlugin = require('./plugins/access-level');
+
+const cidr = new Schema({
+  value: {
+    type: String,
+    required: true,
+    validate: cidrValidator,
+  },
+  min: Decimal128,
+  max: Decimal128,
+  v6: String,
+});
+
+const ipToInt = addr => (addr.toBuffer().readBigUInt64LE(addr.kind() === 'ipv4' ? 8 : 0, true));
+
+cidr.post('validate', function setCIDRValues() {
+  const CIDR = createCIDR(this.value);
+  try {
+    this.v6 = CIDR.toString({ format: 'v4-mapped' });
+    this.min = ipToInt(parse(CIDR.first().toString({ format: 'v4-mapped' }))).toString();
+    this.max = ipToInt(parse(CIDR.last().toString({ format: 'v4-mapped' }))).toString();
+  } catch (e) {
+    this.v6 = CIDR.toString({ format: 'v6' });
+    this.min = ipToInt(CIDR.first()).toString();
+    this.max = ipToInt(CIDR.last()).toString();
+  }
+});
 
 const schema = new Schema({
   name: {
@@ -16,9 +43,8 @@ const schema = new Schema({
     type: String,
     trim: true,
   },
-  ipAddresses: {
-    type: [String],
-    validate: ipValidator,
+  cidrs: {
+    type: [cidr],
     default: () => [],
   },
   domains: {
