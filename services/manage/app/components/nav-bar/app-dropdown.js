ec7eabe98e8c20451cd61506ce881928d08e2210
Jacob Bare <jacob@limit0.io>
2019-06-13 15:12:34 -0500

    Create app dropdown component

---
+++ services/manage/app/components/nav-bar/app-dropdown.js
@@ -0,0 +1,19 @@
+import Component from '@ember/component';
+import { computed } from '@ember/object';
+import { inject } from '@ember/service';
+
+export default Component.extend({
+  tagName: 'li',
+  classNames: ['nav-item', 'dropdown', 'dropdown--org'],
+
+  context: inject(),
+
+  active: computed('context.appId', function() {
+    if (this.context.appId) return 'active';
+    return null;
+  }),
+
+  apps: computed('context.{apps.[],appId}', function() {
+    return this.context.apps.filter(app => app.id !== this.context.appId);
+  }),
+});
