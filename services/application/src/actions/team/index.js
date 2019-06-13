cebbe7c12e5611c50f4d91932c8a8491de678327
Josh Worden <solocommand@gmail.com>
2019-06-13 09:31:47 -0500

    Query for team by IP address

--- services/application/src/actions/team/index.js
+++ services/application/src/actions/team/index.js
@@ -1,10 +1,12 @@
 const { findById, find, listForApp } = require('@base-cms/id-me-utils').actions;
 const create = require('./create');
+const findByIp = require('./find-by-ip');
 const Team = require('../../mongodb/models/team');
 
 module.exports = {
   create,
   find: ({ query, fields }) => find(Team, { query, fields }),
   findById: ({ id, fields }) => findById(Team, { id, fields }),
+  findByIp,
   listForApp: ({ id, fields }) => listForApp(Team, { id, fields }),
 };
