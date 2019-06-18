63a0b4b775e6fef515a8cea6586690181103cbf6
Jacob Bare <jacob@limit0.io>
2019-06-18 10:12:38 -0500

    Move team list fragment to common graphql folder

--- services/manage/app/routes/manage/orgs/view/apps/view/teams.js
+++ services/manage/app/routes/manage/orgs/view/apps/view/teams.js
@@ -1,7 +1,7 @@
 import Route from '@ember/routing/route';
 import AppQueryMixin from '@base-cms/id-me-manage/mixins/app-query';
 import gql from 'graphql-tag';
-import fragment from './teams/list-fragment';
+import fragment from '@base-cms/id-me-manage/graphql/fragments/team-list';
 
 const query = gql`
   query AppTeams {
