e62c3eab7e9d1544b64c64afd0b27f55ec842856
Jacob Bare <jacob@limit0.io>
2019-06-19 13:53:17 -0500

    Create projection directives

--- services/graphql/src/graphql/directives/index.js
+++ services/graphql/src/graphql/directives/index.js
@@ -1,9 +1,13 @@
+const ProjectUsingDirective = require('./project-using');
+const ProjectionDirective = require('./projection');
 const RequiresAppDirective = require('./requires-app');
 const RequiresAppRoleDirective = require('./requires-app-role');
 const RequiresAuthDirective = require('./requires-auth');
 const RequiresOrgRoleDirective = require('./requires-org-role');
 
 module.exports = {
+  projectUsing: ProjectUsingDirective,
+  projection: ProjectionDirective,
   requiresApp: RequiresAppDirective,
   requiresAppRole: RequiresAppRoleDirective,
   requiresAuth: RequiresAuthDirective,
