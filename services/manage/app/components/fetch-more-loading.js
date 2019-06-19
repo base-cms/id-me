bf772aa8ab91a4da6b5a552ceeb8d5e645502c1f
Jacob Bare <jacob@limit0.io>
2019-06-19 14:36:31 -0500

    Create FetchMoreLoading and apply

---
+++ services/manage/app/components/fetch-more-loading.js
@@ -0,0 +1,8 @@
+import Component from '@ember/component';
+import { computed } from '@ember/object';
+import { assign } from '@ember/polyfills';
+import { isArray } from '@ember/array';
+
+export default Component.extend({
+  classNames: ['sk-double-bounce', 'mx-auto'],
+});
