6fdac30879bae62b8fb0df3a5f70b1ff0fb62dda
Jacob Bare <jacob@limit0.io>
2019-06-19 14:40:28 -0500

    Fix lint errors

--- services/manage/app/components/fetch-more-loading.js
+++ services/manage/app/components/fetch-more-loading.js
@@ -1,7 +1,4 @@
 import Component from '@ember/component';
-import { computed } from '@ember/object';
-import { assign } from '@ember/polyfills';
-import { isArray } from '@ember/array';
 
 export default Component.extend({
   classNames: ['sk-double-bounce', 'mx-auto'],
