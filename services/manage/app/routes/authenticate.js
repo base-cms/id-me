bc2fc217d2db50e4fc29bfb9844f0ab167d3e0ee
Josh Worden <solocommand@gmail.com>
2019-06-24 12:17:54 -0500

    Ensure logged-in users are sent to the correct route

--- services/manage/app/routes/authenticate.js
+++ services/manage/app/routes/authenticate.js
@@ -8,6 +8,16 @@ export default Route.extend(UnauthenticatedRouteMixin, {
 
   session: service(),
 
+  async beforeModel(transition) {
+    const route = get(transition, 'to.queryParams.route');
+    const isAuthenticated = await this.session.isAuthenticated;
+    if (isAuthenticated && route) {
+      transition.abort();
+      await this.transitionTo(route);
+    }
+    return this._super(...arguments);
+  },
+
   async model({ token }, transition) {
     const route = get(transition, 'to.queryParams.route');
     if (route) this.session.set('attemptedTransition', this.transitionTo(route));
