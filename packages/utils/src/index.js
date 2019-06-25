1a246c9206d1da62fd7d090cb05b6eb0b1b0ab3e
Josh Worden <solocommand@gmail.com>
2019-06-25 14:12:26 -0500

    Remove buildRegex export

--- packages/utils/src/index.js
+++ packages/utils/src/index.js
@@ -1,12 +1,10 @@
 const actions = require('./actions');
-const buildRegex = require('./build-regex');
 const createLoginToken = require('./create-login-token');
 const mongoose = require('./mongoose');
 const normalizeEmail = require('./nomalize-email');
 
 module.exports = {
   actions,
-  buildRegex,
   createLoginToken,
   mongoose,
   normalizeEmail,
