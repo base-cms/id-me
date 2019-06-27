023d77ddb8434a032aa02e6b5185778316d6d155
Jacob Bare <jacob@limit0.io>
2019-06-27 12:49:55 -0500

    Handle links directly in secondary nav template

--- services/manage/app/components/nav-bar/secondary.js
+++ services/manage/app/components/nav-bar/secondary.js
@@ -1,5 +1,4 @@
 import Component from '@ember/component';
-import { computed } from '@ember/object';
 import { inject } from '@ember/service';
 
 export default Component.extend({
@@ -7,14 +6,4 @@ export default Component.extend({
   classNames: ['navbar', 'navbar--secondary', 'navbar-expand', 'navbar-light'],
 
   context: inject(),
-
-  links: computed('context.appId', function() {
-    const appId = this.get('context.appId');
-    if (!appId) return [];
-    return [
-      { route: 'manage.orgs.view.apps.view.access-levels', text: 'Access Levels', icon: 'shield' },
-      { route: 'manage.orgs.view.apps.view.teams', text: 'Teams', icon: 'slideshare' },
-      { route: 'manage.orgs.view.apps.view.users', text: 'Users', icon: 'man' },
-    ];
-  }),
 });
