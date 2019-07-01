36ac49c510db90908398cd3aaa7e4ac9d40bec3a
Jacob Bare <jacob@limit0.io>
2019-07-01 14:40:35 -0500

    Add search/filter support to teams

--- services/manage/app/routes/manage/orgs/view/apps/view/teams.js
+++ services/manage/app/routes/manage/orgs/view/apps/view/teams.js
@@ -1,11 +1,11 @@
 import Route from '@ember/routing/route';
 import AppQueryMixin from '@base-cms/id-me-manage/mixins/app-query';
-import RouteObservableMixin from '@base-cms/id-me-manage/mixins/route-observable';
+import ListRouteMixin from '@base-cms/id-me-manage/mixins/list-route';
 import gql from 'graphql-tag';
 import fragment from '@base-cms/id-me-manage/graphql/fragments/team-list';
 
-const query = gql`
-  query AppTeams($input: TeamsQueryInput = {}) {
+const teams = gql`
+  query AppTeams($input: TeamsQueryInput!) {
     teams(input: $input) {
       edges {
         node {
@@ -22,15 +22,29 @@ const query = gql`
   ${fragment}
 `;
 
-export default Route.extend(AppQueryMixin, RouteObservableMixin, {
-  async model() {
-    const input = {
-      sort: { field: 'name', order: 'asc' },
-      pagination: { limit: 24 },
-    };
-    const variables = { input };
-    const response = await this.query({ query, variables }, 'teams');
-    this.getController().set('observable', this.getObservable(response));
-    return response;
+const matchTeams = gql`
+  query AppTeamsMatch($input: MatchTeamsQueryInput!) {
+    matchTeams(input: $input) {
+      edges {
+        node {
+          ...TeamListFragment
+        }
+      }
+      pageInfo {
+        hasNextPage
+        endCursor
+      }
+      totalCount
+    }
+  }
+  ${fragment}
+`;
+
+export default Route.extend(AppQueryMixin, ListRouteMixin, {
+  async model(params) {
+    const apollo = this.query.bind(this);
+    const query = { key: 'teams', op: teams };
+    const search = { key: 'matchTeams', op: matchTeams };
+    return this.getResults(apollo, { query, search, params });
   },
 });
