570b4529c842440b5e0304fe15b770e45ffa2090
Jacob Bare <jacob@limit0.io>
2019-07-01 11:32:08 -0500

    Rework button styling/grouping

--- services/manage/app/components/list-controls.js
+++ services/manage/app/components/list-controls.js
@@ -1,5 +1,5 @@
 import Component from '@ember/component';
 
 export default Component.extend({
-  classNames: ['row', 'mb-5'],
+  classNames: ['row'],
 });
--- services/manage/app/components/list-controls/button.js
+++ services/manage/app/components/list-controls/button.js
@@ -2,7 +2,7 @@ import Component from '@ember/component';
 
 export default Component.extend({
   tagName: 'button',
-  classNames: ['btn'],
+  classNames: ['btn', 'btn-list-control'],
   attributeBindings: ['type', 'disabled', 'data-toggle', 'aria-haspopup', 'aria-expanded'],
 
   type: 'button',
