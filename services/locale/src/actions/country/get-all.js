3ba5fc4c1f156933dbb508e9cb8171787fe2088d
Jacob Bare <jacob@limit0.io>
2019-12-11 13:50:04 -0600

    Sort countries by name

--- services/locale/src/actions/country/get-all.js
+++ services/locale/src/actions/country/get-all.js
@@ -3,6 +3,15 @@ const getEmoji = require('./utils/get-emoji');
 
 const { isArray } = Array;
 
+const normalize = name => name.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+const sortByName = (a, b) => {
+  const nameA = normalize(a.name);
+  const nameB = normalize(b.name);
+  if (nameA > nameB) return 1;
+  if (nameA < nameB) return -1;
+  return 0;
+};
+
 module.exports = ({
   lang = 'en',
   prioritize = ['US', 'CA'],
@@ -23,7 +32,12 @@ module.exports = ({
       return o;
     }, {});
 
-  const ordered = { ...top, ...data };
+  const sorted = Object.keys(data)
+    .map(code => ({ code, name: data[code] }))
+    .sort(sortByName)
+    .reduce((o, { code, name }) => ({ ...o, [code]: name }), {});
+
+  const ordered = { ...top, ...sorted };
   return Object.keys(ordered).map((code) => {
     const obj = { code, name: ordered[code] };
     if (!withFlag) return obj;
