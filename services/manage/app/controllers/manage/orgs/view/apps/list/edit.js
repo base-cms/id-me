13fbd21f75ef9562c48a2cb88fce09efadc785c1
Jacob Bare <jacob@limit0.io>
2019-12-11 09:07:30 -0600

    Create initial application edit route+controller

---
+++ services/manage/app/controllers/manage/orgs/view/apps/list/edit.js
@@ -0,0 +1,42 @@
+import Controller from '@ember/controller';
+import ActionMixin from '@base-cms/id-me-manage/mixins/action-mixin';
+import OrgQueryMixin from '@base-cms/id-me-manage/mixins/org-query';
+import gql from 'graphql-tag';
+import { inject } from '@ember/service';
+
+const mutation = gql`
+  mutation OrgAppCreate($input: CreateApplicationMutationInput!) {
+    createApplication(input: $input) {
+      id
+      name
+      description
+    }
+  }
+`;
+
+export default Controller.extend(ActionMixin, OrgQueryMixin, {
+  errorNotifier: inject(),
+
+  actions: {
+    async update(closeModal) {
+      console.log('update!');
+      // try {
+      //   this.startAction();
+      //   const { name, description, email } = this.get('model');
+      //   const input = { name, description, email };
+      //   const variables = { input };
+      //   const refetchQueries = ['Org', 'OrgApps'];
+      //   await this.mutate({ mutation, variables, refetchQueries }, 'createApplication');
+      //   await closeModal();
+      // } catch (e) {
+      //   this.errorNotifier.show(e)
+      // } finally {
+      //   this.endAction();
+      // }
+    },
+
+    returnToAppList() {
+      return this.transitionToRoute('manage.orgs.view.apps.list');
+    },
+  }
+})
