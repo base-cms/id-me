131d0ebd55221ce5b9a8e5381bd6e9736ece151c
Jacob Bare <jacob@limit0.io>
2019-12-11 09:32:04 -0600

    Create updateForId generic action

--- packages/utils/src/actions/index.js
+++ packages/utils/src/actions/index.js
@@ -4,6 +4,7 @@ const listForApp = require('./list-for-app');
 const listForOrg = require('./list-for-org');
 const matchForApp = require('./match-for-app');
 const updateField = require('./update-field');
+const updateForId = require('./update-for-id');
 const updateMany = require('./update-many');
 
 module.exports = {
@@ -13,5 +14,6 @@ module.exports = {
   listForOrg,
   matchForApp,
   updateField,
+  updateForId,
   updateMany,
 };
