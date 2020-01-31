6e94603732f0ee79b57fcbf0091bf8fafa650dd3
Josh Worden <solocommand@gmail.com>
2020-01-31 16:47:48 -0600

    Move logic out of button & into controller

--- services/manage/app/components/button/export.js
+++ services/manage/app/components/button/export.js
@@ -1,43 +1,13 @@
 import Component from '@ember/component';
-import ActionMixin from '@base-cms/id-me-manage/mixins/action-mixin';
-import { ObjectQueryManager } from 'ember-apollo-client';
-import { inject } from '@ember/service';
-import gql from 'graphql-tag';
 
-const mutation = gql`
-  mutation RequestExportAppUsers {
-    exportAppUsers
-  }
-`;
-
-export default Component.extend(ActionMixin, ObjectQueryManager, {
-  errorNotifier: inject(),
-  context: inject(),
-  notifications: inject(),
-
+export default Component.extend({
   tagName: 'button',
   classNames: 'btn btn-xl btn-info btn-circle btn-create',
-  classNameBindings: [
-    'isActionRunning:sk-double-bounce',
-    'isActionRunning:my-auto',
-  ],
   icon: 'download',
   title: 'Export',
+  action: null,
 
   async click() {
-    try {
-      this.startAction();
-      const context = this.context.appQueryContext({});
-      const { exportAppUsers } = await this.apollo.mutate({ mutation, context });
-      if (exportAppUsers === 'ok') {
-        this.notifications.success('Your request has been recieved and will be sent to your email address shortly.');
-      } else {
-        throw new Error('An unknown error occurred.');
-      }
-    } catch (e) {
-      this.errorNotifier.show(e);
-    } finally {
-      this.endAction();
-    }
+    this.action();
   },
 });
--- services/manage/app/controllers/manage/orgs/view/apps/view/users.js
+++ services/manage/app/controllers/manage/orgs/view/apps/view/users.js
@@ -1,6 +1,19 @@
+import { inject } from '@ember/service';
 import ListController from '@base-cms/id-me-manage/controllers/abstract-list';
+import gql from 'graphql-tag';
+import ActionMixin from '@base-cms/id-me-manage/mixins/action-mixin';
+import AppQueryMixin from '@base-cms/id-me-manage/mixins/app-query';
 
-export default ListController.extend({
+const mutation = gql`
+  mutation RequestExportAppUsers {
+    exportAppUsers
+  }
+`;
+
+export default ListController.extend(ActionMixin, AppQueryMixin, {
+  errorNotifier: inject(),
+  notifications: inject(),
+
   init() {
     this._super(...arguments);
     this.set('sortOptions', [
@@ -18,5 +31,23 @@ export default ListController.extend({
       { key: 'familyName', label: 'Last Name' },
     ]);
     this.set('field', 'email');
+  },
+
+  actions: {
+    async export() {
+      try {
+        this.startAction();
+        const { exportAppUsers } = await this.mutate({ mutation });
+        if (exportAppUsers === 'ok') {
+          this.notifications.success('Your request has been recieved and will be sent to your email address shortly.');
+        } else {
+          throw new Error('An unknown error occurred.');
+        }
+      } catch (e) {
+        this.errorNotifier.show(e);
+      } finally {
+        this.endAction();
+      }
+    },
   },
 });
