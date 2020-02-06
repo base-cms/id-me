6d4e88c7ac274f23e6dc229db97a602fbf7f9dd0
Jacob Bare <jacob@limit0.io>
2020-02-06 14:30:45 -0600

    Add filter by category support

--- services/locale/src/actions/region/get-all-for.js
+++ services/locale/src/actions/region/get-all-for.js
@@ -1,9 +1,11 @@
 const { createRequiredParamError } = require('@base-cms/micro').service;
 const regions = require('../../regions');
 
-module.exports = ({ countryCode }) => {
+module.exports = ({ countryCode, category }) => {
   if (!countryCode) throw createRequiredParamError('countryCode');
   const data = regions[countryCode.toUpperCase()];
   if (!data) return [];
-  return Object.values(data);
+  const values = Object.values(data);
+  if (!category) return values;
+  return values.filter(v => v.category === category);
 };
