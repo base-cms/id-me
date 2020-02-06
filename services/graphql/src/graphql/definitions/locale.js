d02953f598d112168cd69909a5f75ee5a174610e
Jacob Bare <jacob@limit0.io>
2020-02-06 14:31:01 -0600

    Add LocaleRegion types and query

--- services/graphql/src/graphql/definitions/locale.js
+++ services/graphql/src/graphql/definitions/locale.js
@@ -4,6 +4,16 @@ module.exports = gql`
 
 extend type Query {
   localeCountries(input: LocaleCountriesQueryInput = {}): [LocaleCountry!]! @requiresApp
+  localeRegionsForCountry(input: LocaleRegionsForCountryQueryInput = {}): [LocaleRegion!]! @requiresApp
+}
+
+enum LocaleRegionCategory {
+  Capital
+  District
+  OutlyingArea
+  Province
+  State
+  Territory
 }
 
 type LocaleCountry {
@@ -12,10 +22,23 @@ type LocaleCountry {
   flag: String
 }
 
+type LocaleRegion {
+  id: String! # a combination of the country+region code, e.g. US-WI
+  code: String! # the region code, e.g. WI
+  name: String! # the region name, e.g. Wisconsin
+  category: LocaleRegionCategory! # the region category, e.g. State
+  country: LocaleCountry! # the country information
+}
+
 input LocaleCountriesQueryInput {
   lang: String = "en"
   prioritize: [String!]! = ["US", "CA"]
   withFlag: Boolean = true
+}
+
+input LocaleRegionsForCountryQueryInput {
+  countryCode: String!
+  category: LocaleRegionCategory
 }
 
 `;
--- services/graphql/src/graphql/resolvers/locale.js
+++ services/graphql/src/graphql/resolvers/locale.js
@@ -1,8 +1,19 @@
 const { localeService } = require('@base-cms/id-me-service-clients');
 
 module.exports = {
+  /**
+   *
+   */
   LocaleCountry: {
     id: country => country.code,
+  },
+
+  /**
+   *
+   */
+  LocaleRegion: {
+    id: region => region.code,
+    code: region => region.regionCode,
   },
 
   Query: {
@@ -12,6 +23,14 @@ module.exports = {
     localeCountries: (_, { input }) => {
       const { lang, prioritize, withFlag } = input;
       return localeService.request('country.getAll', { lang, prioritize, withFlag });
+    },
+
+    /**
+     *
+     */
+    localeRegionsForCountry: (_, { input }) => {
+      const { countryCode, category } = input;
+      return localeService.request('region.getAllFor', { countryCode, category });
     },
   },
 };
