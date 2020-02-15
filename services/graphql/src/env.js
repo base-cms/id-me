dfe7a052a5f4e8fc5cc2b9646caaf4943b98ab9e
Jacob Bare <jacob@limit0.io>
2020-02-15 09:43:00 -0600

    Rename env port descriptions

--- services/graphql/src/env.js
+++ services/graphql/src/env.js
@@ -10,8 +10,8 @@ const { nonemptystr } = validators;
 
 module.exports = cleanEnv(process.env, {
   ENGINE_API_KEY: nonemptystr({ desc: 'The Apollo Engine API key', devDefault: '(unset)' }),
-  EXTERNAL_PORT: port({ desc: 'The external port that express is exposed on.', default: 80 }),
-  INTERNAL_PORT: port({ desc: 'The internal port that express will run on.', default: 80 }),
+  EXTERNAL_PORT: port({ desc: 'The external port that the service is exposed on.', default: 80 }),
+  INTERNAL_PORT: port({ desc: 'The internal port that the service will run on.', default: 80 }),
   MONGO_DSN: nonemptystr({ desc: 'The MongoDB DSN to connect to.' }),
   NEW_RELIC_ENABLED: bool({ desc: 'Whether New Relic is enabled.', default: true, devDefault: false }),
   NEW_RELIC_LICENSE_KEY: nonemptystr({ desc: 'The license key for New Relic.', devDefault: '(unset)' }),
--- services/ip/src/env.js
+++ services/ip/src/env.js
@@ -8,8 +8,8 @@ const {
 const { nonemptystr } = validators;
 
 module.exports = cleanEnv(process.env, {
-  EXTERNAL_PORT: port({ desc: 'The external port that express is exposed on.', default: 80 }),
-  INTERNAL_PORT: port({ desc: 'The internal port that express will run on.', default: 80 }),
+  EXTERNAL_PORT: port({ desc: 'The external port that the service is exposed on.', default: 80 }),
+  INTERNAL_PORT: port({ desc: 'The internal port that the service will run on.', default: 80 }),
   NEW_RELIC_ENABLED: bool({ desc: 'Whether New Relic is enabled.', default: true, devDefault: false }),
   NEW_RELIC_LICENSE_KEY: nonemptystr({ desc: 'The license key for New Relic.', devDefault: '(unset)' }),
 });
