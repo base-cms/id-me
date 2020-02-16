9556363a8425560e72a0974f09846157e8e89035
Jacob Bare <jacob@limit0.io>
2020-02-16 10:19:42 -0600

    Update application service to use json server

--- services/application/src/env.js
+++ services/application/src/env.js
@@ -2,11 +2,14 @@ const {
   cleanEnv,
   validators,
   bool,
+  port,
 } = require('@base-cms/env');
 
 const { nonemptystr } = validators;
 
 module.exports = cleanEnv(process.env, {
+  EXTERNAL_PORT: port({ desc: 'The external port that the service is exposed on.', default: 80 }),
+  INTERNAL_PORT: port({ desc: 'The internal port that the service will run on.', default: 80 }),
   MONGO_DSN: nonemptystr({ desc: 'The MongoDB DSN to connect to.' }),
   NEW_RELIC_ENABLED: bool({ desc: 'Whether New Relic is enabled.', default: true, devDefault: false }),
   NEW_RELIC_LICENSE_KEY: nonemptystr({ desc: 'The license key for New Relic.', devDefault: '(unset)' }),
