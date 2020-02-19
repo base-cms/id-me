e44f273efe74dd937ff9dd4dffac2133f0fabd89
Jacob Bare <jacob@limit0.io>
2020-02-19 14:03:42 -0600

    Format validation errors

--- packages/utils/src/mongoose/handle-error.js
+++ packages/utils/src/mongoose/handle-error.js
@@ -2,6 +2,13 @@ const isDuplicateKeyError = require('./is-duplicate-key-error');
 const isValidationError = require('./is-validation-error');
 
 module.exports = (createError, e, statusCode = 422) => {
-  if (isValidationError(e) || isDuplicateKeyError(e)) return createError(statusCode, e.message);
+  if (isValidationError(e)) {
+    const fields = e.errors && typeof e.errors === 'object' ? e.errors : {};
+    const messages = Object.keys(fields).map(name => fields[name]);
+    const error = createError(422, `${messages.join('\n')}`, e);
+    error.isValidationError = true;
+    return error;
+  }
+  if (isDuplicateKeyError(e)) return createError(statusCode, e.message, e);
   return e;
 };
