9484e61d5df7a04e423062e36f72f103383f0d82
Jacob Bare <jacob@limit0.io>
2020-02-19 14:03:50 -0600

    Customize error messages for all clients

--- packages/service-clients/src/index.js
+++ packages/service-clients/src/index.js
@@ -3,8 +3,17 @@ const env = require('./env');
 const services = require('./services');
 const createEnvVar = require('./create-env-var');
 
+const onCreateError = ({
+  json,
+  res,
+  createError,
+}) => {
+  const { message } = json;
+  return createError(res.status, `${message}`);
+};
+
 module.exports = services.reduce((o, { key, name }) => {
   const varName = createEnvVar(key);
   const url = env[varName];
-  return { ...o, [`${key}Service`]: client.json({ url, name }) };
+  return { ...o, [`${key}Service`]: client.json({ url, name, onCreateError }) };
 }, {});
