2cd2d889799b62d5fd6443c1a71e0cd25afc0212
Jacob Bare <jacob@limit0.io>
2020-03-04 14:19:50 -0600

    Add Organization.updateForId action

--- services/organization/src/actions/index.js
+++ services/organization/src/actions/index.js
@@ -1,3 +1,6 @@
+const { updateForId } = require('@identity-x/utils').actions;
+
+const Organization = require('../mongodb/models/organization');
 const create = require('./create');
 const findById = require('./find-by-id');
 const updateField = require('./update-field');
@@ -6,4 +9,5 @@ module.exports = {
   create,
   findById,
   updateField,
+  updateForId: params => updateForId(Organization, params),
 };
