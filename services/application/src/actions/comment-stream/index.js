6ab9b46d9c06fff55a0a67f774df79540683ec47
Jacob Bare <jacob@limit0.io>
2020-03-07 15:55:01 -0600

    Create comment-stream.findByIdentifier action

--- services/application/src/actions/comment-stream/index.js
+++ services/application/src/actions/comment-stream/index.js
@@ -5,10 +5,12 @@ const {
   matchForApp,
 } = require('@identity-x/utils').actions;
 const create = require('./create');
+const findByIdentifier = require('./find-by-identifier');
 const CommentStream = require('../../mongodb/models/comment-stream');
 
 module.exports = {
   create,
+  findByIdentifier,
   find: ({ query, fields }) => find(CommentStream, { query, fields }),
   findById: ({ id, fields }) => findById(CommentStream, { id, fields }),
   listForApp: params => listForApp(CommentStream, params),
