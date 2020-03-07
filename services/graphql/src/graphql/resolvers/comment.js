50e7a472a14c8e4d6a0583d8ec3f9601645ad28b
Jacob Bare <jacob@limit0.io>
2020-03-07 17:30:18 -0600

    Add rel resolvers for comments

--- services/graphql/src/graphql/resolvers/comment.js
+++ services/graphql/src/graphql/resolvers/comment.js
@@ -4,6 +4,22 @@ module.exports = {
   /**
    *
    */
+  Comment: {
+    id: comment => comment._id,
+    /**
+     *
+     */
+    stream: comment => applicationService.request('comment-stream.findById', { id: comment.streamId }),
+
+    /**
+     *
+     */
+    user: comment => applicationService.request('user.findById', { id: comment.appUserId }),
+  },
+
+  /**
+   *
+   */
   Query: {
     /**
      *
@@ -12,7 +28,7 @@ module.exports = {
       const { identifier } = input;
       const applicationId = app.getId();
       // @todo need to display unapproved and banned comments _only_ for the logged in user.
-      return applicationService.request('listForStream', { applicationId, identifier });
+      return applicationService.request('comment.listForStream', { applicationId, identifier });
     },
   },
 };
