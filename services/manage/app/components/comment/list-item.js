7503bd1e37f7a1c392da17b826de6a4cafdbff46
Jacob Bare <jacob@limit0.io>
2020-03-11 14:42:54 -0500

    Rework buttons on comment list

--- services/manage/app/components/comment/list-item.js
+++ services/manage/app/components/comment/list-item.js
@@ -4,21 +4,27 @@ import gql from 'graphql-tag';
 import ActionMixin from '@identity-x/manage/mixins/action-mixin';
 import AppQueryMixin from '@identity-x/manage/mixins/app-query';
 
-const denyComment = gql`
-  mutation DenyComment($id: String!) {
-    denyComment(input: { id: $id }) {
+const setCommentApproved = gql`
+  mutation SetCommentApproved($id: String!, $value: Boolean!) {
+    setCommentApproved(input: { id: $id, value: $value }) {
       id
       approved
     }
   }
 `;
 
-const approveComment = gql`
-  mutation ApproveComment($id: String!) {
-    approveComment(input: { id: $id }) {
+const setCommentFlagged = gql`
+  mutation SetCommentFlagged($id: String!, $value: Boolean!) {
+    setCommentFlagged(input: { id: $id, value: $value }) {
       id
-      approved
+      flagged
     }
+  }
+`;
+
+const deleteComment = gql`
+  mutation DeleteComment($id: String!) {
+    deleteComment(input: { id: $id })
   }
 `;
 
@@ -28,18 +34,57 @@ export default Component.extend(ActionMixin, AppQueryMixin, {
 
   node: null,
 
+  isTogglingApproval: false,
+  isTogglingFlag: false,
+  isDeleting: false,
+  isTogglingUserBan: false,
+
   actions: {
     async toggleApproval() {
       try {
-        this.startAction();
-        const mutation = this.node.approved ? denyComment : approveComment;
-        const key = this.node.approved ? 'denyComment' : 'approveComment';
-        const variables = { id: this.node.id };
-        await this.mutate({ mutation, variables }, key);
+        this.set('isTogglingApproval', true);
+        const variables = { id: this.node.id, value: !this.node.approved };
+        await this.mutate({ mutation: setCommentApproved, variables }, 'setCommentApproved');
       } catch (e) {
         this.errorNotifier.show(e);
       } finally {
-        this.endAction();
+        if (!this.isDestroyed) this.set('isTogglingApproval', false);
+      }
+    },
+
+    async toggleFlag() {
+      try {
+        this.set('isTogglingFlag', true);
+        const variables = { id: this.node.id, value: !this.node.flagged };
+        await this.mutate({ mutation: setCommentFlagged, variables }, 'setCommentFlagged');
+      } catch (e) {
+        this.errorNotifier.show(e);
+      } finally {
+        if (!this.isDestroyed) this.set('isTogglingFlag', false);
+      }
+    },
+
+    async deleteComment() {
+      try {
+        this.set('isDeleting', true);
+        const variables = { id: this.node.id };
+        await this.mutate({ mutation: deleteComment, variables, refetchQueries: ['AppComments'] }, 'deleteComment');
+      } catch (e) {
+        this.errorNotifier.show(e);
+      } finally {
+        if (!this.isDestroyed) this.set('isDeleting', false);
+      }
+    },
+
+    async toggleUserBan() {
+      try {
+        this.set('isTogglingFlag', true);
+        // const variables = { id: this.node.id, value: !this.node.user.banned };
+        // await this.mutate({ mutation: setCommentFlagged, variables }, 'setCommentFlagged');
+      } catch (e) {
+        this.errorNotifier.show(e);
+      } finally {
+        if (!this.isDestroyed) this.set('isTogglingFlag', false);
       }
     },
   },
