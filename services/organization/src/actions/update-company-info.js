82bb86480c092febdf427e91132a9f8a8da04c6e
Jacob Bare <jacob@limit0.io>
2020-03-27 13:24:16 -0500

    Explicitly set each company key/value

--- services/organization/src/actions/update-company-info.js
+++ services/organization/src/actions/update-company-info.js
@@ -4,11 +4,11 @@ const Organization = require('../mongodb/models/organization');
 
 module.exports = async ({
   id,
-  payload,
+  payload = {},
 } = {}) => {
   if (!id) throw createRequiredParamError('id');
   const org = await Organization.findById(id);
   if (!org) throw createError(404, `No organization found for ID ${id}.`);
-  org.set('company', payload);
+  Object.keys(payload).forEach(key => org.set(`company.${key}`, payload[key]));
   return org.save();
 };
