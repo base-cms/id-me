f0bc308d7be4463a09352f23fbc8a27e06f2f6fe
Jacob Bare <jacob@parameter1.com>
2021-01-25 12:42:34 -0600

    Create initial custom form create modal

---
+++ services/manage/app/controllers/manage/orgs/view/apps/view/fields/create.js
@@ -0,0 +1,41 @@
+import Controller from '@ember/controller';
+import ActionMixin from '@identity-x/manage/mixins/action-mixin';
+import AppQueryMixin from '@identity-x/manage/mixins/app-query';
+import gql from 'graphql-tag';
+import { inject } from '@ember/service';
+
+const mutation = gql`
+  mutation AppSelectFieldCreate($input: CreateSelectFieldMutationInput!) {
+    createSelectField(input: $input) {
+      id
+    }
+  }
+`;
+
+export default Controller.extend(ActionMixin, AppQueryMixin, {
+  errorNotifier: inject(),
+
+  actions: {
+    async create(closeModal) {
+      try {
+        this.startAction();
+        const {
+          name,
+          label,
+        } = this.get('model');
+        const input = {
+          name,
+          label,
+        };
+        const variables = { input };
+        const refetchQueries = ['AppFields'];
+        await this.mutate({ mutation, variables, refetchQueries }, 'createSelectField');
+        await closeModal();
+      } catch (e) {
+        this.errorNotifier.show(e);
+      } finally {
+        this.endAction();
+      }
+    },
+  },
+});
